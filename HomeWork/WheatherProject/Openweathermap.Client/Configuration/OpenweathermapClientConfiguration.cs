bab0081a3aaa0a8180830ba1700c133d18895bb9
PhiseySt <phstas2016@rambler.ru>
2021-06-05 20:11:26 +0300

    add project Openweathermap.Client

---
+++ HomeWork/WheatherProject/Openweathermap.Client/Configuration/OpenweathermapClientConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Refit;
+using System;
+using System.Net.Http;
+
+
+namespace Openweathermap.Client.Configuration
+{
+    public static class OpenweathermapClientConfiguration
+    {
+        public static IServiceCollection AddOpenweathermapClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.TryAddTransient(_ => RestService.For<IOpenweathermapClient>(new HttpClient(
+                new HttpClientHandler { ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true })
+            {
+                BaseAddress = new Uri(configuration["ServiceUrls:OpenweathermapService"])
+            }));
+
+            return services;
+        }
+     }
+}
---
+++ HomeWork/WheatherProject/Openweathermap.Client/IOpenweathermapClient.cs
@@ -0,0 +1,18 @@
+﻿using Openweathermap.Models;
+using Refit;
+using System.Collections.Generic;
+
+namespace Openweathermap.Client
+{
+    public interface IOpenweathermapClient
+    {
+        [Get("weather/temperature/{cityname}/{metric}")]
+        public WeatherModel GetWeatherTemperature(string cityName = "Kazan", string metric = "celsius");
+
+        [Get("weather/wind/{cityName}")]
+        WindModel GetWeatherWind(string cityName = "Kazan");
+
+        [Get("weather/feature/{cityname}/{metric}")]
+        IEnumerable<WeatherModel> GetWeatherTemperatures5Days(string cityName = "Kazan", string metric = "celsius");
+    }
+}
